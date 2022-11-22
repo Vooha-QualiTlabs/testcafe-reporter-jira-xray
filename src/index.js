87609856abb38b7c9796b2f4f354b482bc224063
Murad Alwan <free1soft@gmail.com>
2022-11-22 19:56:16 +0300

    fix duplicated test-execution, show test results in the console

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
+var xray = require("../test/utils/xray");
 
-var xray = require( '../test/utils/xray');
-
-require('dotenv').config();
+require("dotenv").config();
 
 let executionId;
 
@@ -15,118 +14,218 @@ let osType;
 
 let env;
 
-module.exports = function () {  
-               
+module.exports = function () {
     return {
-        noColors: true,
+        noColors: false,
+        startTime: null,
+        afterErrorList: false,
+        testCount: 0,
+        skipped: 0,
 
-        async reportTaskStart (startTime, userAgents, testCount) {
+        async reportTaskStart(startTime, userAgents, testCount) {
             this.startTime = startTime;
             this.testCount = testCount;
-    
-            date = startTime.toString().split(' ');
-            browser = userAgents[0].split(' ')[0];
-            os = userAgents[0].split('/')[1];              
-        },
-    
-        async reportFixtureStart (name, path, meta) {
-            this.currentFixtureName = name;
-        },
 
-        async reportTestStart (/* name, meta */) {
-           
-        },
-    
-        async reportTestDone (name, testRunInfo, meta) {
+            date = startTime.toString().split(" ");
+            browser = userAgents[0].split(" ")[0];
+            os = userAgents[0].split("/")[1];
 
             // Create Test Execution
-            if (os.includes('mac') || os.includes('OS X')) {
-                osType = 'Mac';    
-                env = `${osType} ${browser}`;        
-            }
-            else if (os.includes('Windows')) {
-                osType = 'Windows';
+            if (os.includes("mac") || os.includes("OS X")) {
+                osType = "Mac";
                 env = `${osType} ${browser}`;
-            }
-            else if (os.includes('iOS') || os.includes('Android')) {
-                var deviceName = process.env['MOBILE_DEVICE_NAME'];
+            } else if (os.includes("Windows")) {
+                osType = "Windows";
+                env = `${osType} ${browser}`;
+            } else if (os.includes("iOS") || os.includes("Android")) {
+                var deviceName = process.env["MOBILE_DEVICE_NAME"];
 
-                osType = os.includes('iOS') ? 'ios' : 'android';
+                osType = os.includes("iOS") ? "ios" : "android";
                 env = `${deviceName} ${browser}`;
+            } else env = `${os} ${browser}`;
+
+            const day = `${date[1]}-${date[2]}-${date[3]}`;
+
+            executionId = await xray.createTestExecution(osType, env, day);
+
+            // write to console
+            this.setIndent(1)
+                .useWordWrap(true)
+                .write(this.chalk.bold("Running tests in:"))
+                .newline();
+
+            userAgents.forEach((ua) => {
+                this.write(`- ${this.chalk.blue(ua)}`).newline();
+            });
+        },
+
+        async reportFixtureStart(name, path, meta) {
+            this.currentFixtureName = name;
+            this.setIndent(1).useWordWrap(true);
+
+            if (this.afterErrorList) this.afterErrorList = false;
+            else this.newline();
+
+            this.write(name).newline();
+        },
+
+        async reportTestStart(/* name, meta */) {},
+
+        async reportTestDone(name, testRunInfo, meta) {
+            // write the test run info to the console
+            var hasErr = !!testRunInfo.errs.length;
+            var symbol = null;
+            var nameStyle = null;
+
+            if (testRunInfo.skipped) {
+                this.skipped++;
+
+                symbol = this.chalk.cyan("-");
+                nameStyle = this.chalk.cyan;
+            } else if (hasErr) {
+                symbol = this.chalk.red.bold(this.symbols.err);
+                nameStyle = this.chalk.red.bold;
+            } else {
+                symbol = this.chalk.green(this.symbols.ok);
+                nameStyle = this.chalk.grey;
             }
-            else 
-                env = `${os} ${browser}`;
-            
-             
-            const day = `${date[1]}-${date[2]}-${date[3]}`;      
 
-            executionId = await xray.createTestExecution(osType, env, day);  
+            var title = `${symbol} ${nameStyle(name)}`;
 
-            const errors      = testRunInfo.errs;      
-            const hasErrors   = !!errors.length;    
-            const result      = hasErrors ? 'failed' : 'passed';        
-            
+            this.setIndent(1).useWordWrap(true);
+
+            if (testRunInfo.unstable) title += this.chalk.yellow(" (unstable)");
+
+            if (testRunInfo.screenshotPath)
+                title += ` (screenshots: ${this.chalk.underline.grey(
+                    testRunInfo.screenshotPath
+                )})`;
+
+            this.write(title);
+
+            if (hasErr) this._renderErrors(testRunInfo.errs);
+
+            this.afterErrorList = hasErr;
+
+            this.newline();
+            // export to xray
+            const errors = testRunInfo.errs;
+            const hasErrors = !!errors.length;
+            const result = hasErrors ? "failed" : "passed";
+
             // Get JIRA issue Id for the Test
-            const testKey = name.split(' ')[0];        
+            const testKey = name.split(" ")[0];
             const testIssueId = await xray.getIssueId(testKey);
-    
+
             // Get Test Run Id for the Test in Test Execution
-            var getTestRunData = await xray.getTestRun(testIssueId, executionId);
+            var getTestRunData = await xray.getTestRun(
+                testIssueId,
+                executionId
+            );
             var testRunId;
 
-            if (getTestRunData.data.getTestRun != null) 
+            if (getTestRunData.data.getTestRun != null)
                 testRunId = getTestRunData.data.getTestRun.id;
             else {
                 await xray.addTestToTestExecution(testIssueId, executionId);
-                getTestRunData = await xray.getTestRun(testIssueId, executionId);
+                getTestRunData = await xray.getTestRun(
+                    testIssueId,
+                    executionId
+                );
                 testRunId = getTestRunData.data.getTestRun.id;
             }
-    
-            // Get Test Execution result
-            var testRunStatus = ''; 
 
-            if (result == 'failed') 
-                testRunStatus = 'FAILED';
-            else if (testRunInfo.skipped) 
-                testRunStatus = 'SKIPPED';
-            else 
-                testRunStatus = 'PASSED';
-              
-    
+            // Get Test Execution result
+            var testRunStatus = "";
+
+            if (result == "failed") testRunStatus = "FAILED";
+            else if (testRunInfo.skipped) testRunStatus = "SKIPPED";
+            else testRunStatus = "PASSED";
+
             // Update the Test Run
             await xray.updateTestRunStatus(testRunId, testRunStatus);
-              
-            // Get Error message if failed
-            var testRunError = '';
-            var errorsInfo =  testRunInfo.errs;
 
-            if (testRunStatus == 'FAILED') {                      
-                errorsInfo.forEach(function (error) {          
+            // Get Error message if failed
+            var testRunError = "";
+            var errorsInfo = testRunInfo.errs;
+
+            if (testRunStatus == "FAILED") {
+                errorsInfo.forEach(function (error) {
                     testRunError += error.errMsg;
-                });                
+                });
             }
-    
+
             // Add artifacts for Failures
-            if (testRunStatus == 'FAILED') {
+            if (testRunStatus == "FAILED") {
                 // Add Error message as comments to Test Run
                 await xray.addCommentToTestRun(testRunId, testRunError);
-                
-                // Add Evidence Screenshots to Test Run                
-                for (let i = 1; i <= testRunInfo.screenshots.length; i++) {                    
-                    var imagePaths = testRunInfo.screenshots[i - 1].screenshotPath;
+
+                // Add Evidence Screenshots to Test Run
+                for (let i = 1; i <= testRunInfo.screenshots.length; i++) {
+                    var imagePaths =
+                        testRunInfo.screenshots[i - 1].screenshotPath;
                     var imageData = await xray.base64_encode(imagePaths);
 
                     await xray.addEvidenceToTestRun(testRunId, imageData, i);
                 }
-            }  
-            
+            }
         },
-    
-        async reportTaskDone (/*endTime, passed, warnings, result*/) {
-            
-        }
-    
+
+        async reportTaskDone(endTime, passed, warnings) {
+            var durationMs = endTime - this.startTime;
+            var durationStr = this.moment
+                .duration(durationMs)
+                .format("h[h] mm[m] ss[s]");
+            var footer =
+                passed === this.testCount
+                    ? this.chalk.bold.green(`${this.testCount} passed`)
+                    : this.chalk.bold.red(
+                          `${this.testCount - passed}/${this.testCount} failed`
+                      );
+
+            footer += this.chalk.grey(` (${durationStr})`);
+
+            if (!this.afterErrorList) this.newline();
+
+            this.setIndent(1).useWordWrap(true);
+
+            this.newline().write(footer).newline();
+
+            if (this.skipped > 0) {
+                this.write(
+                    this.chalk.cyan(`${this.skipped} skipped`)
+                ).newline();
+            }
+
+            if (warnings.length) this._renderWarnings(warnings);
+        },
+        _renderErrors(errs) {
+            this.setIndent(3).newline();
+
+            errs.forEach((err, idx) => {
+                var prefix = this.chalk.red(`${idx + 1}) `);
+
+                this.newline()
+                    .write(this.formatError(err, prefix))
+                    .newline()
+                    .newline();
+            });
+        },
+
+        _renderWarnings(warnings) {
+            this.newline()
+                .setIndent(1)
+                .write(this.chalk.bold.yellow(`Warnings (${warnings.length}):`))
+                .newline();
+
+            warnings.forEach((msg) => {
+                this.setIndent(1)
+                    .write(this.chalk.bold.yellow(`--`))
+                    .newline()
+                    .setIndent(2)
+                    .write(msg)
+                    .newline();
+            });
+        },
     };
 };
-
-
