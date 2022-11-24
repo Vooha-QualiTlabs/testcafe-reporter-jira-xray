4b9a1b9dd1469066c2eb0b9ef618da30244acca5
VoohaN <vooha.nallamothu@qualitlabs.com>
2022-11-24 15:18:59 +0530

    updates with linting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-var xray = require("../test/utils/xray");
+var xray = require('../test/utils/xray');
 
-require("dotenv").config();
+require('dotenv').config();
 
 let executionId;
 
@@ -16,33 +16,36 @@ let env;
 
 module.exports = function () {
     return {
-        noColors: false,
-        startTime: null,
+        noColors:       false,
+        startTime:      null,
         afterErrorList: false,
-        testCount: 0,
-        skipped: 0,
+        testCount:      0,
+        skipped:        0,
 
-        async reportTaskStart(startTime, userAgents, testCount) {
+        async reportTaskStart (startTime, userAgents, testCount) {
             this.startTime = startTime;
             this.testCount = testCount;
 
-            date = startTime.toString().split(" ");
-            browser = userAgents[0].split(" ")[0];
-            os = userAgents[0].split("/")[1];
+            date = startTime.toString().split(' ');
+            browser = userAgents[0].split(' ')[0];
+            os = userAgents[0].split('/')[1];
 
             // Create Test Execution
-            if (os.includes("mac") || os.includes("OS X")) {
-                osType = "Mac";
+            if (os.includes('mac') || os.includes('OS X')) {
+                osType = 'Mac';
                 env = `${osType} ${browser}`;
-            } else if (os.includes("Windows")) {
-                osType = "Windows";
+            }
+            else if (os.includes('Windows')) {
+                osType = 'Windows';
                 env = `${osType} ${browser}`;
-            } else if (os.includes("iOS") || os.includes("Android")) {
-                var deviceName = process.env["MOBILE_DEVICE_NAME"];
+            }
+            else if (os.includes('iOS') || os.includes('Android')) {
+                var deviceName = process.env['MOBILE_DEVICE_NAME'];
 
-                osType = os.includes("iOS") ? "ios" : "android";
+                osType = os.includes('iOS') ? 'ios' : 'android';
                 env = `${deviceName} ${browser}`;
-            } else env = `${os} ${browser}`;
+            }
+            else env = `${os} ${browser}`;
 
             const day = `${date[1]}-${date[2]}-${date[3]}`;
 
@@ -51,7 +54,7 @@ module.exports = function () {
             // write to console
             this.setIndent(1)
                 .useWordWrap(true)
-                .write(this.chalk.bold("Running tests in:"))
+                .write(this.chalk.bold('Running tests in:'))
                 .newline();
 
             userAgents.forEach((ua) => {
@@ -59,7 +62,7 @@ module.exports = function () {
             });
         },
 
-        async reportFixtureStart(name, path, meta) {
+        async reportFixtureStart (name, path, meta) {
             this.currentFixtureName = name;
             this.setIndent(1).useWordWrap(true);
 
@@ -69,9 +72,9 @@ module.exports = function () {
             this.write(name).newline();
         },
 
-        async reportTestStart(/* name, meta */) {},
+        async reportTestStart (/* name, meta */) {},
 
-        async reportTestDone(name, testRunInfo, meta) {
+        async reportTestDone (name, testRunInfo, meta) {
             // write the test run info to the console
             var hasErr = !!testRunInfo.errs.length;
             var symbol = null;
@@ -80,12 +83,14 @@ module.exports = function () {
             if (testRunInfo.skipped) {
                 this.skipped++;
 
-                symbol = this.chalk.cyan("-");
+                symbol = this.chalk.cyan('-');
                 nameStyle = this.chalk.cyan;
-            } else if (hasErr) {
+            }
+            else if (hasErr) {
                 symbol = this.chalk.red.bold(this.symbols.err);
                 nameStyle = this.chalk.red.bold;
-            } else {
+            }
+            else {
                 symbol = this.chalk.green(this.symbols.ok);
                 nameStyle = this.chalk.grey;
             }
@@ -94,12 +99,13 @@ module.exports = function () {
 
             this.setIndent(1).useWordWrap(true);
 
-            if (testRunInfo.unstable) title += this.chalk.yellow(" (unstable)");
+            if (testRunInfo.unstable) title += this.chalk.yellow(' (unstable)');
 
-            if (testRunInfo.screenshotPath)
+            if (testRunInfo.screenshotPath) {
                 title += ` (screenshots: ${this.chalk.underline.grey(
                     testRunInfo.screenshotPath
                 )})`;
+            }
 
             this.write(title);
 
@@ -111,10 +117,10 @@ module.exports = function () {
             // export to xray
             const errors = testRunInfo.errs;
             const hasErrors = !!errors.length;
-            const result = hasErrors ? "failed" : "passed";
+            const result = hasErrors ? 'failed' : 'passed';
 
             // Get JIRA issue Id for the Test
-            const testKey = name.split(" ")[0];
+            const testKey = name.split(' ')[0];
             const testIssueId = await xray.getIssueId(testKey);
 
             // Get Test Run Id for the Test in Test Execution
@@ -136,27 +142,27 @@ module.exports = function () {
             }
 
             // Get Test Execution result
-            var testRunStatus = "";
+            var testRunStatus = '';
 
-            if (result == "failed") testRunStatus = "FAILED";
-            else if (testRunInfo.skipped) testRunStatus = "SKIPPED";
-            else testRunStatus = "PASSED";
+            if (result == 'failed') testRunStatus = 'FAILED';
+            else if (testRunInfo.skipped) testRunStatus = 'SKIPPED';
+            else testRunStatus = 'PASSED';
 
             // Update the Test Run
             await xray.updateTestRunStatus(testRunId, testRunStatus);
 
             // Get Error message if failed
-            var testRunError = "";
+            var testRunError = '';
             var errorsInfo = testRunInfo.errs;
 
-            if (testRunStatus == "FAILED") {
+            if (testRunStatus == 'FAILED') {
                 errorsInfo.forEach(function (error) {
                     testRunError += error.errMsg;
                 });
             }
 
             // Add artifacts for Failures
-            if (testRunStatus == "FAILED") {
+            if (testRunStatus == 'FAILED') {
                 // Add Error message as comments to Test Run
                 await xray.addCommentToTestRun(testRunId, testRunError);
 
@@ -171,17 +177,17 @@ module.exports = function () {
             }
         },
 
-        async reportTaskDone(endTime, passed, warnings) {
+        async reportTaskDone (endTime, passed, warnings) {
             var durationMs = endTime - this.startTime;
             var durationStr = this.moment
                 .duration(durationMs)
-                .format("h[h] mm[m] ss[s]");
+                .format('h[h] mm[m] ss[s]');
             var footer =
                 passed === this.testCount
                     ? this.chalk.bold.green(`${this.testCount} passed`)
                     : this.chalk.bold.red(
-                          `${this.testCount - passed}/${this.testCount} failed`
-                      );
+                        `${this.testCount - passed}/${this.testCount} failed`
+                    );
 
             footer += this.chalk.grey(` (${durationStr})`);
 
@@ -199,7 +205,7 @@ module.exports = function () {
 
             if (warnings.length) this._renderWarnings(warnings);
         },
-        _renderErrors(errs) {
+        _renderErrors (errs) {
             this.setIndent(3).newline();
 
             errs.forEach((err, idx) => {
@@ -212,7 +218,7 @@ module.exports = function () {
             });
         },
 
-        _renderWarnings(warnings) {
+        _renderWarnings (warnings) {
             this.newline()
                 .setIndent(1)
                 .write(this.chalk.bold.yellow(`Warnings (${warnings.length}):`))
@@ -220,7 +226,7 @@ module.exports = function () {
 
             warnings.forEach((msg) => {
                 this.setIndent(1)
-                    .write(this.chalk.bold.yellow(`--`))
+                    .write(this.chalk.bold.yellow('--'))
                     .newline()
                     .setIndent(2)
                     .write(msg)
